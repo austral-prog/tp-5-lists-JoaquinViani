a2c5bda53ac2531e4d29f9ed486c94cf26a88eb0
Joaquin Viani <Joaquinviani@gmail.com>
2026-04-07 11:15:17 -0300

    actualizacion

--- exercise_list_of_lists.py
+++ exercise_list_of_lists.py
@@ -2,8 +2,8 @@
 
 def list_of_lists(lista_de_listas):
     lista_de_listas[0] = lista_de_listas [0][0:2]
-    lista_de_listas[0] = lista_de_listas [1][1:4]
-    lista_de_listas[0] = lista_de_listas [2][-2:]
+    lista_de_listas[1] = lista_de_listas [1][1:4]
+    lista_de_listas[2] = lista_de_listas [2][-2:]
     
     return lista_de_listas
 

a6f4ff046978ad3311c9c3887771f893d221a932
Joaquin Viani <Joaquinviani@gmail.com>
2026-04-07 11:07:09 -0300

    actualizacion

--- exercise_concatenate_lists.py
+++ exercise_concatenate_lists.py
@@ -3,7 +3,4 @@
 def concatenate_lists(lista1, lista2):
     lista1 = lista1 + lista2
     print(lista1)
-
-concatenate_lists([1, 2, 3], [4, 5, 6])
-concatenate_lists([], [1, 2, 3])
-concatenate_lists([1, 2], ["a", "b"])
+    return lista1

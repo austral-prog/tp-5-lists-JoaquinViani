48a34e3f00b8c31610fb0627066d69580c54079f
Joaquin Viani <Joaquinviani@gmail.com>
2026-04-07 11:13:08 -0300

    actualizacion

--- exercise_list_of_lists.py
+++ exercise_list_of_lists.py
@@ -1,10 +1,10 @@
 # Ejercicio 12: Manipular lista de listas
 
 def list_of_lists(lista_de_listas):
-    print(lista_de_listas [0][0:2])
-    print(lista_de_listas [1][1:4])
-    print(lista_de_listas [2][-1:-3:-1])
+    lista_de_listas[0] = lista_de_listas [0][0:2]
+    lista_de_listas[0] = lista_de_listas [1][1:4]
+    lista_de_listas[0] = lista_de_listas [2][-1:-3:-1]
+    
+    return lista_de_listas
 
-list_of_lists([[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]])
-list_of_lists([[], [4, 5, 6], [10, 11, 12]])
-list_of_lists( [[1, 2], [], [12]])
+

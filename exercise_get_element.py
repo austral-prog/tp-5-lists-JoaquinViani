9a27a95c693d5b46c521d2954cb4aef9a00e4664
Joaquin Viani <Joaquinviani@gmail.com>
2026-04-07 11:33:45 -0300

    Actualizacion final

--- exercise_get_element.py
+++ exercise_get_element.py
@@ -1,6 +1,6 @@
 # Ejercicio 2: Obtener elemento en posición específica
 
 def get_element(lista, indice):
-    if indice >= len(lista) or -indice <= len(lista):
+    if indice >= len(lista) or indice <= -len(lista):
         return None
     return lista[indice]

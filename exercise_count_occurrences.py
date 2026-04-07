01dda8e3c1df7c8a83325c6dad45bd79e1a7d2ba
Joaquin Viani <Joaquinviani@gmail.com>
2026-04-07 11:09:09 -0300

    actualizacion

--- exercise_count_occurrences.py
+++ exercise_count_occurrences.py
@@ -1,8 +1,4 @@
 # Ejercicio 7: Contar ocurrencias de un elemento
 
 def count_occurrences(lista, elemento):
-        print(lista.count(elemento))
-
-count_occurrences([1, 2, 2, 3, 2, 4], 2)
-count_occurrences([1, 2, 3, 4, 5], 10)
-count_occurrences(["red", "blue", "red", "green", "red"], "red")
+        return lista.count(elemento)
